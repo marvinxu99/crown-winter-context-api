0959aeefd824fddbcd4a8d3a3b46785dae97dccd
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-10-21 19:35:09 -0700

    commit

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react';
-=======
-import React from 'react';
-import { connect } from 'react-redux';
-//import { Redirect } from 'react-router-dom';
-//import { createStructuredSelector } from 'reselect';
->>>>>>> master
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import './collection.styles.scss';
 
-<<<<<<< HEAD
 import CollectionsContext from '../../contexts/collections/collections.context';
-=======
-const CollectionPage = ({ collection }) => {
-  //console.log(match.params);
-  
-  //if(!collection) {
-  //console.log('need re-direct');
-  //  return(<Redirect to='/shop' />);
-  //}
->>>>>>> master
 
 
 const CollectionPage = ({ match }) => {
@@ -44,6 +27,34 @@ const CollectionPage = ({ match }) => {
       </div>
     </div>
   )
+
+  // return(
+  //   <CollectionsContext.Consumer>
+  //   {
+  //     collections => {
+  //       const collection = collections[match.params.collectionId];
+  //       const { title, items } = collection;
+  //       return(
+  //         <div className='collection-page'>
+  //           <h2 className='title'>{ title }</h2>
+  //           <div className='items'>
+  //             {
+  //               items.map(item => (
+  //                 <CollectionItem 
+  //                   className='collection-item' 
+  //                   key={ item.id } 
+  //                   item={ item } 
+  //                 /> 
+  //               ))
+  //             }
+  //           </div>
+  //         </div>
+  //       )
+  //     }
+  //   }
+  //   </CollectionsContext.Consumer>
+  // )
+  
 }
 
 export default CollectionPage;
